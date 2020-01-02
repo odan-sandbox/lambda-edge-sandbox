c8d9851d7c461f3232c1a7c47bb738c56a7bc25c
odan <odan3240@gmail.com>
2020-01-02 23:10:03 +0900

    lambda-edge 用のアプリケーションを追加

---
+++ lambda-edge/src/app.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import basicAuth from "express-basic-auth";
+
+const app = express();
+
+app.use(
+  basicAuth({
+    users: { admin: "supersecret" },
+    challenge: true
+  })
+);
+
+app.get("/", (_, res) => {
+  res.send("Hello!");
+});
+
+app.listen(3000, () => console.log("start: http://localhost:3000"));
